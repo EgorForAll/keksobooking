54af28815c1cf7ff5eda6b9e807fc6339cbd1567
egor <eia.web.ss@gmail.com>
2023-01-18 10:28:25 +0300

    random dates inserted in HTML

--- js/modules/dates.js
+++ js/modules/dates.js
@@ -12,6 +12,24 @@ const AVATAR = [
   '../img/avatars/user11.png',
 ];
 
+const TITLE = [
+  'Hampton by Hilton',
+  'Hotel Space',
+  'Park inn',
+  'Hotel Volga',
+  'Hotel Emerald',
+  'Emerald',
+  'Red field',
+];
+
+const DESCRIPTION = [
+  'Изысканный и элегантный курортный отель «Загородный клуб Скандинавия» расположен в живописном приморском городе Сестрорецк, в нескольких минутах ходьбы от побережья Балтийского моря.',
+  'Этот отель расположен в 5 минутах ходьбы от Невского проспекта и станции метро «Маяковская». Неподалеку находится Московский вокзал, музей Достоевского и различные кафе.',
+  'Отель «Истра Холидей» находится на берегу Истринского водохранилища, всего в 45 км от Москвы.',
+  'Этот 5-звездочный отель расположен на берегу Истринского водохранилища в поселке Рождествено, в 60 км к северо-западу от Москвы.',
+  'Отель с 2 ресторанами и фитнес-центром расположен в Москве, рядом с Измайловским парком, в 3 минутах ходьбы от станций метро «Партизанская» и станции МЦК «Измайлово».',
+];
+
 const TYPE = [
   'palace',
   'flat',
@@ -47,4 +65,4 @@ const PHOTOS = [
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'
 ];
 
-export {AVATAR, TYPE, PHOTOS, FEATURES, CHECKIN, CHECKOUT};
+export {AVATAR, TYPE, PHOTOS, FEATURES, CHECKIN, CHECKOUT, TITLE, DESCRIPTION};
