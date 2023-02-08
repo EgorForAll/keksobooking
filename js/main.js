db7d9d947a081310a899357149c1853e70f3c124
egor <eia.web.ss@gmail.com>
2023-02-08 12:37:18 +0300

    photos could be loaded now

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ import './modules/map.js';
 import './modules/filters.js';
 import './modules/slider.js';
 import './modules/api.js';
+import './modules/photo.js';
 import { getData } from './modules/api.js';
 import { getMarkers } from './modules/map.js';
 import { setUserFormSubmit } from './modules/form.js';
---
+++ js/modules/photo.js
@@ -0,0 +1,42 @@
+// Загузка аватарки
+const avatar = document.querySelector('#avatar');
+const avatarPreview = document.querySelector('.ad-form-header__preview').firstElementChild;
+
+const FILE_TYPES = ['gif', 'jpg', 'jpeg', 'png'];
+
+avatar.addEventListener('change', () => {
+  const file = avatar.files[0];
+  const fileName = file.name.toLowerCase();
+
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    avatarPreview.src = URL.createObjectURL(file);
+  }
+});
+
+// Загрузить фото жилья
+
+const photo = document.querySelector('#images');
+const photoPreview = document.querySelector('.ad-form__photo');
+photoPreview.setAttribute('style', 'display: flex');
+photoPreview.setAttribute('style', 'text-align: center');
+
+photo.addEventListener('change', () => {
+  const file = photo.files[0];
+  const fileName = file.name.toLowerCase();
+  const img = document.createElement('img');
+  img.setAttribute('style', 'width: 100%');
+  img.setAttribute('style', 'height: 100%');
+  photoPreview.appendChild(img);
+
+  const matches = FILE_TYPES.some((it) => {
+    return fileName.endsWith(it);
+  });
+
+  if (matches) {
+    img.src = URL.createObjectURL(file);
+  }
+});
